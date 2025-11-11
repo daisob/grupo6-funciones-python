01e36a7274c8776c11427e7242d425198576aed6
Maria Iglesias <maru.eiglesias@gmail.com>
2025-11-11 13:42:17 -0300

    se ageraga modulo, numero par y promedio

---
+++ funciones/promedio.py
@@ -0,0 +1,7 @@
+# funciones/promedio.py
+
+def promedio(numeros):
+    """Devuelve el promedio de una lista de números."""
+    if not numeros:
+        return None
+    return sum(numeros) / len(numeros)
---
+++ test/test_numeroPar.py
@@ -0,0 +1,6 @@
+# tests/test_es_par.py
+from funciones.numeroPar import es_par
+
+def test_es_par():
+    assert es_par(4) is True
+    assert es_par(7) is False
